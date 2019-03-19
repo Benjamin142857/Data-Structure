4dd0ef1f6209f0a8b94e5eec843dece962ba5b7e
Benjamin142857 <Benjamin142857@outlook.com>
2019-03-19 18:07:20 +0800

    20190319

---
+++ 栈-LinkStack/LinkStack_func.h
@@ -0,0 +1,94 @@
+//
+// Created by Benjamin142857 on 3/17/2019.
+//
+
+#ifndef L2_LINKSTACK_FUNC_H
+#define L2_LINKSTACK_FUNC_H
+
+#include "LinkStack_def.h"
+#include "malloc.h"
+
+
+// 创建结点
+PNode create_node(PNode next_p, DataType x) {
+    // 1.定义指针
+    PNode p;
+    // 2.分配内存
+    p = (PNode)malloc(sizeof(struct Node));
+    // 3.判断节点是否创建成功
+    if(p != NULL) {
+        p->info = x;
+        p->link = next_p;
+    }
+    else {
+        printf("结点创建失败\n");
+    }
+
+}
+
+
+// 创建空LinkStack
+PLinkStack create_LinkStack(void) {
+    // 1.定义指针
+    PLinkStack pls;
+    // 2.分配内存
+    pls = (PLinkStack)malloc(sizeof(struct LinkStack));
+    // 3.判断链表栈是否创建成功
+    if(pls != NULL) {
+        pls->top = NULL;
+    }
+    else {
+        printf("链表栈创建失败\n");
+    }
+    // 4.返回LinkStack指针
+    return pls;
+}
+
+
+// 判断是否空栈 (0-非空，1-空)
+int is_empty(PLinkStack pls) {
+    return (pls->top == NULL);
+}
+
+
+// 进栈
+int push(PLinkStack pls, DataType x) {
+    // 1.创建一个结点
+    PNode p = create_node(pls->top, x);
+    // 2.判断结点是否创建成功
+    if(p != NULL) {
+        pls->top = p;
+    }
+    else {
+        printf("进栈失败");
+    }
+}
+
+
+// 出栈
+void pop(PLinkStack pls) {
+    // 1.定义空结点指向栈顶元素作为临时容器，然后free
+    PNode p = pls->top;
+    // 2.判断是否空栈
+    if (is_empty(pls)) {
+        printf("该链表栈为空\n");
+    }
+    else {
+        pls->top = pls->top->link;
+        free(p);
+    }
+}
+
+
+// 取栈顶
+DataType get_top(PLinkStack pls) {
+    // 1.判断是否空栈
+    if (is_empty(pls)) {
+        printf("该链表栈为空\n");
+    }
+    else {
+        return pls->top->info;
+    }
+}
+
+#endif //L2_LINKSTACK_FUNC_H
---
+++ 栈-LinkStack/main.c
@@ -0,0 +1,62 @@
+// 实验2 - 分别基于递归函数与链表栈实现数字字符串转数字整型
+// 第2组 - [古富源，方智威，杜祖铧，邓泽淳]
+// 2019-03-17
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "LinkStack_func.h"
+
+
+// 字符串-名定义
+typedef char String[128];
+
+
+// 题二 - 基于递归函数的数字字符串转数字整型
+long NumStr2NumInt_1(String NumStr, int NumStrLen) {
+    long res;
+    if(NumStrLen==0){
+        return 0;
+    }
+    else {
+        int temp = NumStr[NumStrLen-1] - 48;
+        res = temp + 10*NumStr2NumInt_1(NumStr, NumStrLen-1);
+        return res;
+    }
+
+
+}
+
+
+// 题二 - 基于链表栈的数字字符串转数字整型
+long NumStr2NumInt_2(String NumStr) {
+    // 1.数字字符串从左到右依次入栈
+    PLinkStack pls = create_LinkStack();
+    for(int i=0; i<strlen(NumStr); i++) {
+        push(pls, NumStr[i]);
+    }
+
+    // 2.再依次取栈顶，出栈，字符转Int
+    long res = 0;
+    for(int i=0; i<strlen(NumStr); i++) {
+        int temp = get_top(pls) - 48;
+        res += temp*pow(10, i);
+        pop(pls);
+    }
+
+    // 3.返回
+    return res;
+
+}
+
+
+int main() {
+    String NumStr = "421353242";
+    long res1 = NumStr2NumInt_1(NumStr, strlen(NumStr));
+    long res2 = NumStr2NumInt_2(NumStr);
+    printf("[1] Output result:\n%ld\n\n", res1);
+    printf("[2] Output result:\n%ld\n", res2);
+    return 0;
+}
+
